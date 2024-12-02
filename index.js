c94e315e42b36f7ff8853e2a66327b9e4064f0c8
Olusanmi Pamilerin kehinde <pamilerinolusanmi@gmail.com>
2024-12-02 17:33:09 +0100

    worked on the nav bar

---
+++ index.js
@@ -0,0 +1,7 @@
+function openNav() {
+ let offCanvasNav= document.getElementById("offCanvasNav").classList.add("open");
+}
+
+function closeNav() {
+  document.getElementById("offCanvasNav").classList.remove("open");
+}
